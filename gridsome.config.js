b841c9cc8c5ce6732d608ff88fa08ea205d06719
connor <reillyc3@gmail.com>
2020-04-22 21:47:43 -0500

    contentful hookup

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,13 +3,24 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-
+const tailwindcss = require("tailwindcss")
 module.exports = {
   siteName: 'Connor Reilly Resume',
 
   plugins: [
     {
-      use: 'gridsome-plugin-tailwindcss'
+      use: 'gridsome-plugin-tailwindcss',
+      use: "@gridsome/source-contentful",
+      options: {
+        space: process.env.CONTENTFUL_SPACE,
+        accessToken: process.env.CONTENTFUL_TOKEN,
+        host: "cdn.contentful.com",
+        environment: process.env.CONTENTFUL_ENVIRONMENT,
+        typename: "Contentful"
+      }
+     
+  
+    }
     
       /* These are the default options. You don't need to set any options to get going.
       options: {
@@ -21,6 +32,15 @@ module.exports = {
         shouldTimeTravel: true
       }
       */
-    }
-  ]
+    
+  ],
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          tailwindcss
+        ],
+      },
+    },
+  }
 }
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import "./assets/css/global.css";
 
 config.autoAddCss = false;
 library.add(faGithub, faTwitter)
