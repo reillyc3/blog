6a0eb6e393901860d476ee3e8709a1d4a1575e50
connor <reillyc3@gmail.com>
2020-06-05 14:57:01 -0500

    added projects section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -20,7 +20,42 @@
     </div>
   <section class="container mx-auto pt-6">
     <h1 class="text-2xl font-bold text-primary text-center">Projects</h1>
-
+    <div>
+       <div class="bg-gray-100 lg:py-12 lg:flex lg:justify-center justify-between">
+        <div class="bg-white lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg">
+            <div class="lg:w-1/2">
+                <div class="h-64 bg-cover lg:rounded-lg lg:h-full">
+                  <img src="../images/Udemy image.jpg" alt="" >
+                </div>
+            </div>
+            <div class="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2">
+                <h2 class="text-2xl font-bold text-primary">Udemy Gridsome Course</h2>
+                <p class="mt-4 text-darkgray">Have you ever wanted to build a blog or portfolio site, but didn't know what tech stack to use? This course is for you! I'll walk you through step-by-step, line-by-line, from zero to fully functional blog site.</p>
+                <div class="mt-8">
+                    <a href="https://www.udemy.com/gridsome" class="bg-primary text-white hover:bg-darkgray rounded-full px-5 py-3 font-semibold rounded">View</a>
+                </div>
+            </div>
+        </div>
+    </div> 
+</div>
+   <div>
+       <div class="bg-gray-100 lg:py-12 lg:flex lg:justify-center flex-wrap">
+        <div class="bg-white lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg flex-wrap">
+             <div class="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2 flex-wrap">
+                <h2 class="text-2xl font-bold text-primary">Coming Soon!</h2>
+                <p class="mt-4 text-darkgray">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem modi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dicta tenetur, porro consequuntur saepe accusantium consequatur.</p>
+                <div class="mt-8">
+                    <a href="#" class="bg-primary text-white hover:bg-darkgray rounded-full px-5 py-3 font-semibold rounded">View</a>
+                </div>
+            </div>
+            <div class="lg:w-1/2">
+                <div class="h-64 bg-cover lg:rounded-lg lg:h-full">
+                  <img class="" src="../images/undraw_under_construction_46pa.svg" alt="" >
+                </div>
+            </div>
+        </div>
+    </div> 
+</div>
   </section>	
 	
   
@@ -35,9 +70,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
